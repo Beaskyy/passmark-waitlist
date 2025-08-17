103ba58bea5c2b1982fb2357af8420a7efb625d8
Beaskyy <afeez.akintobi@yahoo.com>
2025-08-17 18:12:07 +0100

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
-import ClientOnly from "@/components/client-only";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,9 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
-        <ClientOnly>{children}</ClientOnly>
-      </body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
